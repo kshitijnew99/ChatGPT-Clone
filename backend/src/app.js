d59ecd2b9276bfc909004fd04d077c44c2a9d59a
kshitij99 <kshitijraoranjannew99@gmail.com>
2026-03-17 04:37:27 +0530

    fix(express): update wildcard route for express 5 paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use('/auth',authRoutes)
 app.use('/chat',chatRoutes)
 
 
-app.get("*",(req,res)=>{
+app.get(/.*/,(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
